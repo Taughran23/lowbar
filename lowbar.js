5994187bfa5d47edb8fbc2d1dd63750edbe3f933
Leo Duffy <leoduffy@hotmail.com>
2017-10-25 15:50:34 +0100

    [each] Implement list and edge case handling

--- lowbar.js
+++ lowbar.js
@@ -14,4 +14,27 @@ _.first = (arr, n) => {
   }
 };
 
+_.last = (arr, n) => {
+  if (!Array.isArray(arr) || arr.length === 0) return undefined;
+  if (n !== undefined && isNaN(n)) return [];
+  if (n === undefined) n = 1;
+
+  const end = arr.slice(arr.length - Math.ceil(n), arr.length);
+  return end.length === 1 ? end[0] : end;
+};
+
+_.each = (list, func, context) => {
+  if (context) func.bind(context);
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      func(list[i], i, list);
+    }
+  } else {
+    for (let key in list) {
+      func(list[key], key, list);
+    }
+  }
+  return list;
+};
+
 module.exports = _;
