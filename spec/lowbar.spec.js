452c7df05282169c4789b5fa2fd5c49c0b2ca9f6
Leo Duffy <leoduffy@hotmail.com>
2017-10-25 15:38:53 +0100

    [first] Add tests for edge cases and two arguments passed

--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -31,4 +31,35 @@ describe('_', function () {
       });
     });
   });
+  describe('#first', function () {
+    it('should be as function', () => {
+      expect(_.first).to.be.a('function');
+    });
+    it('should return undefined when passed an empty array', () => {
+      const result = _.first([]);
+      expect(result).to.equal(undefined);
+    });
+    it('should return the first element in an array when passed one argument', () => {
+      const result = _.first([1, 2, 3, 4]);
+      expect(result).to.equal(1);
+    });
+    it('should return the first n elements in an array if the second argument is defined', () => {
+      const result = _.first([1, 2, 3, 4], 2);
+      expect(result).to.eql([1, 2]);
+    });
+    it('should return an empty array if the second argument is defined but not an integer', () => {
+      const result = _.first([
+        [1, [2]], 2, 3, 4
+      ], 'integer');
+      expect(result).to.eql([]);
+    });
+    it('should return the first n elements in an array, if n is a decimal it should round down', () => {
+      const result = _.first([
+        [1, [2]], 2, 3, 4
+      ], 3.5);
+      expect(result).to.eql([
+        [1, [2]], 2, 3
+      ]);
+    });
+  });
 });
