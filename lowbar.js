e0db576ff079b296ef585bebfd6453dfd71fe5f3
Leo Duffy <leoduffy@hotmail.com>
2017-10-25 15:41:08 +0100

    [first] Implement array and edge case handling

--- lowbar.js
+++ lowbar.js
@@ -4,4 +4,14 @@ _.identity = (val) => {
   return val;
 };
 
+_.first = (arr, n) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return undefined;
+  } else if (n === undefined) {
+    return arr[0];
+  } else {
+    return arr.slice(0, Math.floor(n));
+  }
+};
+
 module.exports = _;
