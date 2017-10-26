3f10a62e6c22f3306adfb600277dabc2f27d490e
Leo Duffy <leoduffy@hotmail.com>
2017-10-26 09:34:47 +0100

    [memoize] Add tests for call count and cache property

--- spec/lowbarAdvanced.spec.js
+++ spec/lowbarAdvanced.spec.js
@@ -30,5 +30,37 @@ describe.only('_', () => {
       expect(spy.callCount).to.equal(1);
     });
   });
+  describe('#memoize', () => {
+    it('should be a function', () => {
+      expect(_.memoize).to.be.a('function');
+    });
+    it('should return the same value that calling the original function returns', () => {
+      const func = (num) => {
+        return num * 2;
+      };
+      const memFunc = _.memoize(func);
+      expect(memFunc(2)).to.equal(func(2));
+    });
+    it('should only call the function once if it has been called previously', () => {
+      const func = (num) => {
+        return Math.pow(num, 3);
+      };
+      const spy = sinon.spy(func);
+      const cubed = _.memoize(spy);
+      cubed(2);
+      cubed(2);
+      expect(spy.callCount).to.equal(1);
+    });
+    it('should return a function that has a cache property', () => {
+      const func = (num) => {
+        return Math.pow(num, 3);
+      };
+      const cubed = _.memoize(func);
+      cubed(2);
+      expect(cubed.cache).to.eql({
+        '2': 8
+      });
+    });
+  });
 });
 
