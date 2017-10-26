bc08b472da8c3b1491e0ffbf3589c21a5935724e
Leo Duffy <leoduffy@hotmail.com>
2017-10-26 09:18:13 +0100

    [extends] Implement object merge

---
+++ lowbar.js
@@ -0,0 +1,180 @@
+const _ = {};
+
+_.identity = (val) => {
+  return val;
+};
+
+_.first = (arr, n) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return undefined;
+  } else if (n === undefined) {
+    return arr[0];
+  } else {
+    return arr.slice(0, Math.floor(n));
+  }
+};
+
+_.last = (arr, n) => {
+  if (!Array.isArray(arr) || arr.length === 0) return undefined;
+  if (n !== undefined && isNaN(n)) return [];
+  if (n === undefined) n = 1;
+
+  const end = arr.slice(arr.length - Math.ceil(n), arr.length);
+  return end.length === 1 ? end[0] : end;
+};
+
+_.each = (list, func, context) => {
+  if (context) func.bind(context);
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      func(list[i], i, list);
+    }
+  } else {
+    for (let key in list) {
+      func(list[key], key, list);
+    }
+  }
+  return list;
+};
+
+_.indexOf = (arr, value, isSorted) => {
+  isSorted = isSorted || false;
+  if (isSorted) {
+    let first = 0,
+      last = arr.length - 1,
+      mid;
+    while (first <= last) {
+      mid = Math.floor((first + last) / 2);
+      if (arr[mid] < value) {
+        first = mid + 1;
+      } else if (arr[mid] > value) {
+        last = mid - 1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) return i;
+  }
+  return -1;
+};
+
+_.filter = (list, predicate, context) => {
+  const result = [];
+  if (context) predicate.bind(context);
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      if (predicate(list[i])) {
+        result.push(list[i]);
+      }
+    }
+  } else {
+    for (let key in list) {
+      if (predicate(list[key])) {
+        result.push(list[key]);
+      }
+    }
+  }
+  return result;
+};
+
+_.reject = (list, predicate, context) => {
+  const result = [];
+  _.each(list, (value, index, obj) => {
+    if (!predicate(value, index, obj)) result.push(value);
+  }, context);
+  return result;
+};
+
+_.uniq = (arr) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (_.indexOf(result, arr[i]) === -1) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+_.map = (list, iteritee, context) => {
+  const result = [];
+  if (typeof iteritee !== 'function') {
+    iteritee = () => undefined;
+  }
+  _.each(list, (value, index, arr) => {
+    result.push(iteritee(value, index, arr));
+  }, context);
+  return result;
+};
+
+_.contains = (list, value, fromIndex) => {
+  if (!Array.isArray(list)) {
+    list = Object.values(list);
+  }
+  if (Number.isInteger(fromIndex)) {
+    list = list.splice(fromIndex, list.length);
+  }
+  if (_.indexOf(list, value) !== -1) return true;
+  return false;
+};
+
+_.pluck = (list, propertyName) => {
+  return _.map(list, (obj) => {
+    return obj[propertyName];
+  });
+};
+
+_.reduce = (list, iteritee, memo, context) => {
+  if (list.length === 0) memo = 0;
+  if (Object.keys(list).length === 0 && list.constructor === Object) {
+    memo = 0;
+  }
+  _.each(list, (value, index) => {
+    if (memo === undefined) {
+      memo = value;
+      iteritee(memo, value, index, list);
+    } else {
+      memo = iteritee(memo, value, index, list);
+    }
+  }, context);
+
+  return memo;
+};
+
+_.every = (list, predicate, context) => {
+  if (context) predicate = predicate.bind(context);
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      if (!predicate(list[i])) return false;
+    }
+    return true;
+  } else {
+    for (let key in list) {
+      if (!predicate(list[key])) return false;
+    }
+    return true;
+  }
+};
+
+_.some = (list, predicate, context) => {
+  if (context) predicate = predicate.bind(context);
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      if (predicate(list[i])) return true;
+    }
+    return false;
+  } else {
+    for (let key in list) {
+      if (predicate(list[key])) return true;
+    }
+    return false;
+  }
+};
+
+_.extends = (destination, source) => {
+  return Object.assign({},destination, source);
+};
+
+module.exports = _;
